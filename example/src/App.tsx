0c58ae4ea43cded85ba416fb97e30d9f7a7eaeaf
Osagie <cj16521>
2025-03-21 20:16:02 +0300

    feat(health-api): add initialization and check permission method

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,32 @@
-import { multiply } from 'rn-samsung-health-data-api';
+import {
+  initializeHealthStore,
+  checkHealthPermissionsGranted,
+} from 'rn-samsung-health-data-api';
 import { Text, View, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [result, setResult] = useState<number | undefined>();
+  const [result, setResult] = useState<string | undefined>();
 
   useEffect(() => {
-    multiply(3, 7).then(setResult);
+    const handleInitialize = async () => {
+      const initData = await initializeHealthStore();
+      if (!initData) {
+        setResult('Error');
+      }
+
+      setResult('Init');
+      console.log('==== HERE');
+
+      try {
+        const granted = await checkHealthPermissionsGranted(['STEPS']);
+        console.log('Permissions Result:', granted);
+      } catch (error) {
+        console.error('Error requesting Samsung Health permissions:', error);
+      }
+    };
+
+    handleInitialize();
   }, []);
 
   return (
