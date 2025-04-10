7e5fa660e02438a1592485f791c659b51aa07d1f
Osagie <cj16521>
2025-04-10 23:41:48 +0300

    feat: fetch step data

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,17 @@
 import {
   initializeHealthStore,
   checkHealthPermissionsGranted,
+  requestHealthPermissions,
+  readStepData,
 } from 'rn-samsung-health-data-api';
 import { Text, View, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
+import { endOfWeek, startOfWeek } from 'date-fns';
 
 export default function App() {
   const [result, setResult] = useState<string | undefined>();
+  const [isPermissionsGranted, setIsPermissionsGrant] =
+    useState<boolean>(false);
 
   useEffect(() => {
     const handleInitialize = async () => {
@@ -16,11 +21,19 @@ export default function App() {
       }
 
       setResult('Init');
-      console.log('==== HERE');
 
       try {
         const granted = await checkHealthPermissionsGranted(['STEPS']);
-        console.log('Permissions Result:', granted);
+        if (granted.allGranted) {
+          setIsPermissionsGrant(true);
+        } else {
+          const requested = await requestHealthPermissions(['STEPS']);
+          if (requested.allGranted) {
+            setIsPermissionsGrant(true);
+          } else {
+            console.log('Permissions Requested:', requested);
+          }
+        }
       } catch (error) {
         console.error('Error requesting Samsung Health permissions:', error);
       }
@@ -28,6 +41,32 @@ export default function App() {
 
     handleInitialize();
   }, []);
+
+  useEffect(() => {
+    const handleFetchSteps = async () => {
+      const todaysDate = new Date();
+
+      const startDate = startOfWeek(todaysDate, { weekStartsOn: 1 });
+      const endDate = endOfWeek(new Date(), { weekStartsOn: 1 });
+
+      const startDateString = startDate.toISOString();
+      const endDateString = endDate.toISOString();
+
+      const stepData = await readStepData({
+        timeRangeFilter: {
+          operator: 'between',
+          startTime: startDateString,
+          endTime: endDateString,
+          timeGroup: 'daily',
+        },
+        ascendingOrder: true,
+      });
+
+      console.log({ stepData });
+    };
+
+    handleFetchSteps();
+  }, [isPermissionsGranted]);
 
   return (
     <View style={styles.container}>
